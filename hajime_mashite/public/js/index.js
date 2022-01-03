463a0fae707ab44c54c8e510983971d691165813
tagaya-hajime <hazime0401@gmail.com>
2022-01-04 05:58:38 +0900

    changes

---
+++ hajime_mashite/public/js/index.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    let card = document.getElementById('card');
+    card.onclick = function () {
+        card.classList.add('erase');
+    };
+};
+
--- hajime_mashite/resources/views/index.blade.php
+++ hajime_mashite/resources/views/index.blade.php
@@ -1,17 +1,32 @@
 <!DOCTYPE html>
 <html lang=ja>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,300&display=swap" rel="stylesheet">
+    <script type="text/javascript" src="js/index.js"></script>
     <title>多賀谷源</title>
 </head>
 
 <body>
     <div id="surface">
-        <img class="card" src="" alt="" title="学生証">
-
+        <div id="card">
+            <img src="img/test.png" alt="" title="学生証">
+            <div class="arrow">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <p class="click">click me</p>
+        </div>
     </div>
+
+
+
+
 
 </body>
 

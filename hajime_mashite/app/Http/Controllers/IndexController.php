45da5ef3f40935807bd30ca87bc0e8c6b87fb220
tagaya-hajime <hazime0401@gmail.com>
2022-01-04 03:34:19 +0900

    change

---
+++ hajime_mashite/app/Http/Controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Nette\Utils\Html;
+
+class IndexController extends Controller
+{
+    public function index(){
+        
+    }
+}
+
--- hajime_mashite/resources/views/index.blade.php
+++ hajime_mashite/resources/views/index.blade.php
@@ -3,11 +3,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="stylesheet" href="css/index.css">
     <title>多賀谷源</title>
 </head>
 
 <body>
+    <div id="surface">
+        <img class="card" src="" alt="" title="学生証">
+
+    </div>
 
 </body>
 

8833cd05dfb3856bc6982fce69ecb475e718e736
tagaya-hajime <hazime0401@gmail.com>
2022-01-05 07:22:22 +0900

    changes

--- hajime_mashite/app/Http/Controllers/IndexController.php
+++ hajime_mashite/app/Http/Controllers/IndexController.php
@@ -11,11 +11,12 @@ class IndexController extends Controller
 {
     public function index(){
         $data_skill = DB::select('select * from skills');
+        $data_portfolio = DB::select('select * from portfolios');
 
 
 
 
-        return view('index', compact('data_skill'));
+        return view('index', compact('data_skill','data_portfolio'));
         
     }
 }
--- hajime_mashite/resources/views/index.blade.php
+++ hajime_mashite/resources/views/index.blade.php
@@ -28,15 +28,6 @@
         <div id="picture">
             <img src="img/test.png" alt="" title="学生証">
         </div>
-        <div class="portfolio contents">
-            <ul>
-                <lh>portfolio</lh>
-                <li>ヒトノオト</li>
-                <li>なりきれ，標識さん！</li>
-                <li>しずのす</li>
-                <p>github→</p>
-            </ul>
-        </div>
         <div class="about contents">
             <ul>
                 <lh>about me</lh>
@@ -46,17 +37,32 @@
                 <p>detail→</p>
             </ul>
         </div>
+        <div class="portfolio contents">
+            <ul>
+                <lh>portfolio</lh>
+                @foreach($data_portfolio as $item)
+                <li class="fukidashi_trigger">{{$item->portfolio_name}}
+                    <span class="fukidashi"><?=$item->portfolio_detail?></span>
+                </li>
+                @endforeach
+                <p>github→</p>
+            </ul>
+        </div>
+
         <div class="skills contents">
             <ul>
                 <lh>skills</lh>
-                <li>HTML&CSS&JavaScript</li>
-                <li>C++,C#</li>
-                <li>PHP</li>
-                <li>Java</li>
+                @foreach($data_skill as $item)
+                <li class="fukidashi_trigger">{{$item->skill_name}}
+                    <span class="fukidashi"><?=$item->skill_detail?></span>
+                </li>
+                @endforeach
 
             </ul>
         </div>
+
         <div class="background"></div>
     </div>
 </body>
+
 </html>

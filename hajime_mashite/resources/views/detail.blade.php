075b90e7578d83c7b0feb02641c0e2552992f20f
tagaya-hajime <hazime0401@gmail.com>
2022-01-26 15:28:50 +0900

    Update detail.blade.php

--- hajime_mashite/resources/views/detail.blade.php
+++ hajime_mashite/resources/views/detail.blade.php
@@ -6,25 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/index.css">
     <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300&family=Merriweather:ital,wght@1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>hajime-detail</title>
 </head>
 
 <body>
     <div id="main">
-        <p class="title">Thanks for visiting my page!</p>
-        <p class="explain">please mouse over the content...</p>
-        <div id="picture">
-            <img src="img/test.png" alt="" title="学生証">
+        <p class="">About me</p>
+        <div class="container">
+    <div class="row">
+        <div class="col-md-6 "><img src="img/me.jpg" class="img-fluid"></div>
+        <div class="col-md-6">
+            <h1>sampletext</h1>
+            <p>samplet extsampletextsam pletextsampletextsamplete xtsampl etextsa mpletextsamplet
+              exts amplet extsamp lete xtsam p letext sampl ete xtsamp letextsample text sam plet ext 
+                sa mp l etext sa mp let exts amplete xtsample textsa mplet extsam ple text
+            </p>
         </div>
-        <div class="skills contents">
-
-        </div>
-        <div class="portfolio contents">
-  
-        </div>
-        <div class="about contents">
-
-        </div>
+    </div>
+</div>
 
 
 
